8cd4cfa3802a7c9071f5bee24190445c71fd4ecc
Matthew Burke <matthewpatrickburke@gmail.com>
2018-09-26 15:06:56 -0600

    added determinant pictures

--- pythonUtils/plottingUtils.py
+++ pythonUtils/plottingUtils.py
@@ -5,6 +5,28 @@ from IPython.display import display, Markdown, Math, Latex
 from mpmath import mp
 import numpy as np
 from .elimination import roundMatrix
+import matplotlib.patches as patches
+
+def plotColumns(A):
+    xmax = max(A[0][:])+1
+    ymax = max(A[1][:])+1
+    xmin = min(A[0][:])-1
+    ymin = min(A[1][:])-1
+    maximum = max(abs(xmax), abs(ymax))
+    minimum = min(ymin, xmin)
+    style="Simple,tail_width=0.5,head_width=4,head_length=8"
+
+    ax = plt.axes()
+    ax.set_xlim(minimum, maximum)
+    ax.set_ylim(minimum, maximum)
+    ax.add_patch(plt.Polygon([[0,0], [A[0][0],A[1][0]], [A[0][0]+A[0][1], A[1][0]+A[1][1]], [A[0][1], A[1][1]]], color='palegreen' , zorder = 2))
+    #ax.add_patch(patches.FancyArrowPatch([A[0][0]/2,A[1][0]/2], [A[0][1]/2,A[1][1]/2],connectionstyle="arc3,rad=.5", arrowstyle=style, color="k", zorder=3))
+    ax.arrow(0, 0, A[0][0], A[1][0], head_width=maximum/20, fc='b', ec='b', length_includes_head = True, zorder = 3)
+    ax.arrow(0, 0, A[0][1], A[1][1], head_width=maximum/20, fc='r', ec='r', length_includes_head = True, zorder = 3)
+    plt.grid(zorder = 0)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.show()
+
 
 mp.dps = 4
 def plot2DSystem(A):
